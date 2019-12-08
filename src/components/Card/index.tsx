d37623b66a2da78dae7867f9fb304eb802b1d7ea
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2019-12-08 09:05:39 +0900

    :fire: Remove material ui components

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,10 +1,4 @@
 import React from "react";
-import {
-  Card as CardComponent,
-  CardHeader,
-  IconButton
-} from "@material-ui/core";
-import DeleteIcon from "@material-ui/icons/Delete";
 import styles from "./index.module.scss";
 
 interface Props {
@@ -15,16 +9,6 @@ interface Props {
 export const Card: React.FC<Props> = ({ title, onClick }) => {
   return (
     <div>
-      <CardComponent>
-        <CardHeader
-          action={
-            <IconButton aria-label="delete" onClick={onClick}>
-              <DeleteIcon />
-            </IconButton>
-          }
-          title={title}
-        />
-      </CardComponent>
       <div className={styles.myCard}>
         <div className={styles.myBody}>{title}</div>
         <div className={styles.myDeleteButton} onClick={onClick}>
